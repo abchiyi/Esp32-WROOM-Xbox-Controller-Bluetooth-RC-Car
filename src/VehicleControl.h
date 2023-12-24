1c95e6391faa76ffb1ceaca5da2456d03078aff3
abchiyi <meme_me2019@outlook.com>
2023-12-24 21:46:41 +0800

    分割功移动转向功能至独立文件

---
+++ src/VehicleControl.h
@@ -0,0 +1,10 @@
+#include <XboxController.h>
+/*
+ @param controller 控制器对象
+ @param channelTurn 转向舵机 pwm 通道
+ @param servoPin 舵机驱动引脚
+ @param channelMove 驱动马达 pwm 通道
+ @param motorPin 驱动电机控制引脚
+ @param motorPinR 驱动电机反转控制引脚
+ */
+void VehicleControlSetup(XboxController *controller, int channelTurn, int servoPin, int channelMove, int motorPin, int motorPinR);
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <Arduino.h>
-
-#include <ESP32Servo.h>
-#include <NimBLEDevice.h>
-#include <XboxController.h>
+#include <VehicleControl.h>
 
 XboxController xboxController;
-Servo servo;
 
 // 定义控制 Pin
 #define PIN_MOVE 12    // 移动控制
@@ -16,12 +12,6 @@ Servo servo;
 #define PIN_R_LIGHT 32 // 右转向灯
 #define CHANNEL_LIGHT_L 1
 #define CHANNEL_LIGHT_R 2
-
-const int width = 65535;
-const int deadZone = 4500;
-const int LStart = width / 2 - deadZone / 2;
-const int RStart = width / 2 + deadZone / 2;
-const int JoyLength = 256;
 
 bool LightTurnL = false;
 bool LightTurnR = false;
@@ -160,20 +150,6 @@ void TaskIndicatorLightControl(void *pt)
     btnLB_old = btnLB;
     btnRB_old = btnRB;
 
-    // Serial.print("R :");
-    // Serial.print(LightTurnR);
-    // Serial.print(" Old R :");
-    // Serial.print(btnLB_old);
-    // Serial.print(" L :");
-    // Serial.print(LightTurnL);
-    // Serial.print(" Old L :");
-    // Serial.print(btnLB_old);
-    // Serial.print(" H :");
-    // Serial.print(HazardLight);
-    // Serial.print(" B :");
-    // Serial.print(btnLB && btnRB);
-    // Serial.print(" ch :");
-    // Serial.println(changed);
     vTaskDelay(5);
   }
 }
@@ -226,77 +202,6 @@ void TaskStatusLight(void *pt)
   }
 }
 
-/* 将摇杆输入值转换为角度并应用到舵机 */
-void Turn(int joy, bool reverse = false)
-{
-
-  // 计算打杆量
-  int t_joy = joy <= LStart ? joy : joy - deadZone;
-  int value = round(double(t_joy) * (double(JoyLength) / double(LStart)));
-
-  // 转换杆量为角度
-  double step = 90.000 / double(JoyLength);
-  int ang = 180 - value * step;
-
-  // 写入角度
-  servo.write(ang);
-}
-
-/* 验证要写入的参数是否已经写入过了 */
-void ifAnalogWrite(uint8_t pin, int value)
-{
-  static int oldValue = 0;
-  if (value != oldValue)
-  {
-    analogWrite(pin, value);
-    oldValue = value;
-  }
-}
-
-/* 车辆移动
-  @param reverse 后退
-  @param stop 停止
-  @trig 油门控制
-*/
-void Move(int trig, bool reverse, bool stop)
-{
-
-  if (!stop)
-  {
-    ifAnalogWrite(PIN_MOVE, round(trig / 4));
-    reverse
-        ? digitalWrite(PIN_MOVE_R, 1)
-        : digitalWrite(PIN_MOVE_R, 0);
-  }
-  else
-  {
-    ifAnalogWrite(PIN_MOVE, 0);
-    digitalWrite(PIN_MOVE_R, 0);
-  }
-}
-
-/* 车辆控制 */
-void VehicleControl()
-{
-  if (xboxController.connected)
-  {
-    auto controller = xboxController.get();
-
-    const int LT = xboxController.get().trigLT;
-    const int RT = xboxController.get().trigRT;
-
-    LT   ? Move(LT, true, false)  // 倒车
-    : RT ? Move(RT, false, false) // 前进
-         : Move(0, false, true);  // 停止
-
-    // 执行转向动作
-    const int joy = controller.joyLHori;
-    joy <= LStart   ? Turn(joy)     // 从左摇杆起始值计算旋转90°~0°
-    : joy >= RStart ? Turn(joy)     // 从右摇杆起始值计算旋转90°~180°
-                    : Turn(LStart); // 复位至90°
-  }
-}
-
 void setup()
 {
   Serial.begin(115200);
@@ -309,9 +214,6 @@ void setup()
   pinMode(PIN_TURN, OUTPUT);
   pinMode(PIN_L_LIGHT, OUTPUT);
   pinMode(PIN_R_LIGHT, OUTPUT);
-  // 转向设置
-  servo.setPeriodHertz(50);
-  servo.attach(PIN_TURN, 50, 2500);
 
   ledcSetup(CHANNEL_LIGHT_L, 2000, 8);
   ledcAttachPin(33, CHANNEL_LIGHT_L);
@@ -323,9 +225,10 @@ void setup()
   xTaskCreate(TaskIndicatorLightControl, "IndicatorLightControl", 1024, NULL, 1, NULL);
 
   xTaskCreate(TaskStatusLight, "status light", 1024, NULL, 1, NULL);
+
+  VehicleControlSetup(&xboxController, 4, PIN_TURN, 5, PIN_MOVE, PIN_MOVE_R);
 }
 
 void loop()
 {
-  VehicleControl();
 }
