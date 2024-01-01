0bae11aa06c5f037c6ffc58d9dc9786336ceda87
abchiyi <meme_me2019@outlook.com>
2024-01-01 14:00:21 +0800

    change

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <Arduino.h>
-
 #include <ESP32Servo.h>
-#include <VehicleLight.h>
-// #include <VehicleControl.h>
 #include <XboxController.h>
 
 Servo TurnServo;
@@ -22,23 +19,33 @@ XboxController Controller;
 #define PIN_R_LIGHT 33         // 右转向灯
 
 // PWM 通道
+#define CHANNEL_LIGHT_L 2         // 左转向灯
+#define CHANNEL_LIGHT_R 3         // 右转向灯
 #define CHANNEL_HEADLIGHT 6       // 大灯PWM通道
 #define CHANNEL_STOPLIGHT 7       // 刹车灯PWM通道
 #define CHANNEL_STATUSLIGHT 1     // 状态灯
 #define CHANNEL_REVERSING_LIGHT 8 // 倒车灯PWM通道
 
-#define CHANNEL_LIGHT_L 2
-#define CHANNEL_LIGHT_R 3
-
 bool DISTANT_LIGHT = false; // 远光
+bool LOW_BEAM = false;      // 近光
 bool WIDTH_LAMP = false;    // 示宽灯
 int REVERSING_LIGHT = 0;    // 倒车灯
-bool LOW_BEAM = false;      // 近光
+int HeadLight = 0;          // 大灯   亮度0~255
+int StopLight = 0;          // 刹车灯 亮度0~255
 bool BRAKE = false;         // 刹车
-int HeadLight = 0;          // 大灯亮度
-int StopLight = 0;          // 大灯亮度
+bool LightTurnL = false;    // 左转灯
+bool LightTurnR = false;    // 右转灯
+bool HazardLight = false;   // 危险报警灯
 
-// 车辆移动
+/* 设置 pwm 输出引脚 */
+void setPWMPin(int pin, int pwmChannel)
+{
+  pinMode(pin, OUTPUT);
+  ledcSetup(pwmChannel, 2000, 8);
+  ledcAttachPin(pin, pwmChannel);
+}
+
+// 移动任务
 void TaskMove(void *pt)
 {
   bool HOLD_RT = false;
@@ -103,6 +110,7 @@ void TaskMove(void *pt)
   }
 }
 
+// 转向任务
 void TaskTurn(void *pt)
 {
   const int width = 65535;
@@ -130,39 +138,33 @@ void TaskTurn(void *pt)
 
       // 写入角度
       TurnServo.write(ang);
-      if (Controller.connected)
-      {
-        Serial.print("Ang : ");
-        Serial.println(ang);
-      }
     }
     else // 在死区内时回正
     {
       TurnServo.write(90);
-      if (Controller.connected)
-      {
-        Serial.print("Ang : ");
-        Serial.println("---");
-      }
     }
 
     vTaskDelay(1);
   }
 }
 
-void VehicleControlSetup(void *controller, int channelTurn, int servoPin, int channelMove, int motorPin, int motorPinR)
+/* 车辆操控设置 转向，移动 */
+void VehicleControlSetup()
 {
-  // 设置马达
-  ledcSetup(channelMove, 2000, 8);
-  ledcAttachPin(motorPin, channelMove);
+
+  // 电调设置
+  pinMode(PIN_MOVE_R, OUTPUT);       // F/R
+  setPWMPin(PIN_MOVE, CHANNEL_MOVE); // motor
 
   // 设置舵机
+  pinMode(PIN_TURN, OUTPUT);
   TurnServo.setPeriodHertz(50);
-  TurnServo.attach(servoPin, 50, 2500);
+  TurnServo.attach(PIN_TURN, 50, 2500);
 
   xTaskCreate(TaskTurn, "Turn", 2048, NULL, 2, NULL);
   xTaskCreate(TaskMove, "Move", 2048, NULL, 2, NULL);
 }
+
 /* 灯光任务 大灯，倒车灯，刹车灯*/
 void TaskLight(void *pt)
 {
@@ -175,7 +177,7 @@ void TaskLight(void *pt)
   }
 }
 
-/* 灯光控制任务  */
+/* 灯光控制任务控制 */
 void TaskLightControll(void *pt)
 {
   bool changed = false;
@@ -256,24 +258,142 @@ void TaskLightControll(void *pt)
   }
 }
 
+/* 状态灯任务 */
+void TaskStatusLight(void *pt)
+{
+  int pwmChannel = CHANNEL_STATUSLIGHT;
+  int resolution = 100;
+  double step = (double)255 / (double)resolution;
+  while (true)
+  {
+    if (XboxController::connected)
+    {
+      ledcWrite(pwmChannel, 255);
+      vTaskDelay(100);
+      ledcWrite(pwmChannel, 0);
+      vTaskDelay(100);
+      ledcWrite(pwmChannel, 255);
+      vTaskDelay(100);
+      ledcWrite(pwmChannel, 0);
+      vTaskDelay(2000);
+    }
+    else
+    {
+      for (int i = 0; i <= 100; i = i + 5)
+      {
+        ledcWrite(pwmChannel, round((double)i * step));
+        vTaskDelay(3);
+      }
+      vTaskDelay(100);
+
+      for (int i = 100; i >= 0; i = i - 5)
+      {
+        ledcWrite(pwmChannel, round((double)i * step));
+        vTaskDelay(3);
+      }
+      vTaskDelay(100);
+    }
+  }
+}
+
+/* 转向灯任务 */
+void TaskIndicatorLight(void *pt)
+{
+  bool increasing = false;
+  int lightLevel = 100;
+
+  const int MAX = 100;
+  const int MIN = 0;
+  const double step = (double)255 / (double)MAX;
+
+  while (true)
+  {
+
+    if (LightTurnL || LightTurnR || HazardLight)
+    {
+      increasing ? lightLevel++ : lightLevel--;
+      increasing = lightLevel >= MAX   ? false
+                   : lightLevel <= MIN ? true
+                                       : increasing;
+    }
+
+    int v = lightLevel <= MIN ? MIN : round((double)lightLevel * step);
+    LightTurnL || HazardLight // 左转向灯
+        ? ledcWrite(CHANNEL_LIGHT_L, v)
+        : ledcWrite(CHANNEL_LIGHT_L, 0);
+
+    LightTurnR || HazardLight // 右转向灯
+        ? ledcWrite(CHANNEL_LIGHT_R, v)
+        : ledcWrite(CHANNEL_LIGHT_R, 0);
+
+    lightLevel == MIN || lightLevel == MAX ? vTaskDelay(200) : vTaskDelay(1);
+  }
+}
+
+/* 转向灯按钮控制任务 */
+void TaskIndicatorLightControl(void *pt)
+{
+  bool changed = false;
+  bool btnLB_old, btnRB_old, btnLB, btnRB;
+
+  while (true)
+  {
+
+    btnLB = Controller.data.btnLB;
+    btnRB = Controller.data.btnRB;
+
+    // 当按下的按钮有变化时重置 changed
+    if (btnLB != btnLB_old || btnRB != btnRB_old)
+    {
+      changed = false;
+    }
+
+    if (btnLB && btnRB)
+    {
+      LightTurnR = false;
+      LightTurnL = false;
+      HazardLight = changed ? HazardLight : !HazardLight;
+      changed = true;
+    }
+    else if (btnLB && !HazardLight && btnLB != btnLB_old)
+    {
+      LightTurnL = changed ? LightTurnL : !LightTurnL;
+      LightTurnR = false;
+      HazardLight = false;
+      changed = true;
+    }
+    else if (btnRB && !HazardLight && btnRB != btnRB_old)
+    {
+      LightTurnR = changed ? LightTurnR : !LightTurnR;
+      LightTurnL = false;
+      HazardLight = false;
+      changed = true;
+    }
+
+    // 在循环结尾处储存本次循环按钮的值以备在下个循环中比对
+    btnLB_old = btnLB;
+    btnRB_old = btnRB;
+
+    vTaskDelay(5);
+  }
+}
+
 /* 设置灯光任务 */
 void LightSetup()
 {
 
-  pinMode(PIN_HEADLIGHT, OUTPUT);
-  ledcSetup(CHANNEL_HEADLIGHT, 2000, 8);
-  ledcAttachPin(PIN_HEADLIGHT, CHANNEL_HEADLIGHT);
-
-  pinMode(PIN_STOPLIGHT, OUTPUT);
-  ledcSetup(CHANNEL_STOPLIGHT, 2000, 8);
-  ledcAttachPin(PIN_STOPLIGHT, CHANNEL_STOPLIGHT);
-
-  pinMode(PIN_REVERSING_LIGHT, OUTPUT);
-  ledcSetup(CHANNEL_REVERSING_LIGHT, 2000, 8);
-  ledcAttachPin(PIN_REVERSING_LIGHT, CHANNEL_REVERSING_LIGHT);
+  setPWMPin(PIN_R_LIGHT, CHANNEL_LIGHT_R);
+  setPWMPin(PIN_L_LIGHT, CHANNEL_LIGHT_L);
+  setPWMPin(PIN_HEADLIGHT, CHANNEL_HEADLIGHT);
+  setPWMPin(PIN_STOPLIGHT, CHANNEL_STOPLIGHT);
+  setPWMPin(PIN_STATUSLIGHT, CHANNEL_STATUSLIGHT);
+  setPWMPin(PIN_REVERSING_LIGHT, CHANNEL_REVERSING_LIGHT);
 
   xTaskCreate(TaskLight, "TaskHeadlight", 1024, NULL, 3, NULL);
+  xTaskCreate(TaskStatusLight, "TaskStatusLight", 1024, NULL, 3, NULL);
   xTaskCreate(TaskLightControll, "TaskHeadlightControll", 1024, NULL, 3, NULL);
+  xTaskCreate(TaskIndicatorLight, "Indicator Light", 1024, NULL, 3, NULL);
+  xTaskCreate(TaskIndicatorLightControl, "IndicatorLightControl", 1024, NULL, 3, NULL);
 }
 
 void setup()
@@ -281,15 +401,8 @@ void setup()
   Serial.begin(115200);
   Controller.connect(NimBLEAddress("XXX"));
 
-  // 初始化针脚
-  pinMode(PIN_MOVE, OUTPUT);
-  pinMode(PIN_MOVE_R, OUTPUT);
-  pinMode(PIN_TURN, OUTPUT);
-
-  StatusLightSetup(PIN_STATUSLIGHT, CHANNEL_STATUSLIGHT);
-  IndicatorLightSetup(&Controller, 12, 12);
   LightSetup();
-  VehicleControlSetup(&Controller, 3, PIN_TURN, 4, PIN_MOVE, PIN_MOVE_R);
+  VehicleControlSetup();
 }
 
 void loop()
